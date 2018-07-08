5031a43c01906e046dfd9c4ad5a3b8d91184d1ff
David Matos <david.matos21@gmail.com>
2018-07-08 18:17:57 +0100

    Fixed clone_missing_projects.sh

--- MACHETE/install/clone_missing_projects.sh
+++ MACHETE/install/clone_missing_projects.sh
@@ -4,11 +4,3 @@ cd ../../
 
 echo "Cloning DepSpacito ..."
 git clone https://github.com/inesc-id/DepSpacito
-
-echo "Cloning Darshana ..."
-git clone https://github.com/inesc-id/darshana
-
-cd darshana
-
-echo "Checkingout alerts-receiver branch ..."
-git checkout alerts-receiver
